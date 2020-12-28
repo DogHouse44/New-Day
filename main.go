097de08eeafbeb9dd88f6c9605d20d190c27d6f9
kyle anderson <75914277+DogHouse44@users.noreply.github.com>
2020-12-28 14:35:01 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import ( 
+  "fmt"
+  t "time" 
+)
+
+func main() {
+	fmt.Println("Hello World")
+  fmt.Println("┇≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┇")
+  fmt.Println("┇  Time Below      ┇")
+  fmt.Println("┇     Made By      ┇")
+  fmt.Println("┇  Doghouse44      ┇")
+  fmt.Println("┇                  ┇")
+  fmt.Println("┇                  ┇")
+  fmt.Println("┇  Message of Day  ┇")
+  fmt.Println("┇                  ┇")
+  fmt.Println("┇  Learn Somthing  ┇")
+  fmt.Println("┇≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┇")
+  fmt.Println(t.Now())
+}
